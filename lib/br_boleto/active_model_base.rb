d32d68b670c11807f76024c40cf8b7509fc2f82c
Bruno M. Mergen <brunomergen@gmail.com>
2016-04-13 16:44:04 -0300

    Criado class para ActiveRecordBase - para manter um padrão das classe e onde contém alguns métodos auxiliáres

---
+++ lib/br_boleto/active_model_base.rb
@@ -0,0 +1,28 @@
+# encoding: utf-8
+module BrBoleto
+	class ActiveModelBase
+		# Seguindo a interface do Active Model para:
+		# * Validações;
+		# * Internacionalização;
+		# * Nomes das classes para serem manipuladas;
+		#
+		include ActiveModel::Model
+
+		def initialize(attributes = {})
+			attributes = default_values.merge!(attributes)
+			assign_attributes(attributes)
+			yield self if block_given?
+		end
+
+		def assign_attributes(attributes)
+			attributes ||= {}
+			attributes.each do |name, value|
+				send("#{name}=", value)
+			end
+		end
+
+		def default_values
+			{}
+		end
+	end
+end
