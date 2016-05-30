1a93cdafc6887cac2361d895362ac2f41380d60c
Bruno M. Mergen <brunomergen@gmail.com>
2016-05-30 00:30:43 -0300

    Update version

--- lib/br_boleto/version.rb
+++ lib/br_boleto/version.rb
@@ -1,7 +1,7 @@
 module BrBoleto
 	module Version
 		MAJOR = 1 #inclui alterações de API e pode quebrar compatibilidade com versões anteriores
-		MINOR = 1 #inclui novas funcionalidades, sem quebrar APIs existentes
+		MINOR = 2 #inclui novas funcionalidades, sem quebrar APIs existentes
 		PATCH = 0 #corrige bugs ou traz melhorias em implementações já existentes
 		CURRENT = "#{MAJOR}.#{MINOR}.#{PATCH}"
 	end
