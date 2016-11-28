45f5a291c210d01bc09939f4611c523caf89ecd8
Ricardo Zanuzzo <ricardo@duobr.com.br>
2016-11-28 10:08:19 -0200

    Configuração retorno CNAB240 Santander

---
+++ test/br_boleto/retorno/cnab240/santander_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+require 'pry'
+describe BrBoleto::Retorno::Cnab240::Santander do
+	subject { BrBoleto::Retorno::Cnab240::Santander.new(file) }
+	# let(:file) { open_fixture('retorno/cnab240/padrao240.ret') }
+
+end
