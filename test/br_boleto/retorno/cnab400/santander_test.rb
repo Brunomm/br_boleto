d0e85c8df29d34571a3b085fd7848978415fca64
Ricardo Zanuzzo <ricardo@duobr.com.br>
2016-11-30 08:28:25 -0200

    Configuração arquivo retorno CNAB400 Santander

---
+++ test/br_boleto/retorno/cnab400/santander_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+describe BrBoleto::Retorno::Cnab400::Santander do
+	subject { BrBoleto::Retorno::Cnab400::Santander.new(file) }
+	# let(:file) { open_fixture('retorno/cnab400/nome.ret') }
+
+end
