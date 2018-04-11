d9d55435f9621003292fc54a7e01b003b0e79365
Bruno M. Mergen <brunomergen@gmail.com>
2018-04-11 17:58:00 -0300

    Upgrate version to 2.2.7

--- lib/br_boleto/version.rb
+++ lib/br_boleto/version.rb
@@ -2,7 +2,7 @@ module BrBoleto
 	module Version
 		MAJOR = 2 #inclui alterações de API e pode quebrar compatibilidade com versões anteriores
 		MINOR = 2 #inclui novas funcionalidades, sem quebrar APIs existentes
-		PATCH = 6 #corrige bugs ou traz melhorias em implementações já existentes
+		PATCH = 7 #corrige bugs ou traz melhorias em implementações já existentes
 		CURRENT = "#{MAJOR}.#{MINOR}.#{PATCH}"
 	end
 end
